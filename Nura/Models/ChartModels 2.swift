679b0bac69b72a92827f11fa3d9fd85309ff6eb2
Jerry <yangzinan127@gmail.com>
2026-05-29 21:22:25 +0800

    fix:删除错误ChartModels

--- Nura/Models/ChartModels 2.swift
+++
@@ -1,6 +0,0 @@
-// ⚠️ DEPRECATED - 此文件已弃用，请删除
-// 所有图表模型已合并到 ChartModels.swift
-// 请从 Xcode 项目中删除此文件以避免编译错误
-
-// This file should be DELETED from your Xcode project
-// All chart models are now in ChartModels.swift
